036f7078e45680b20e0688b883008559fe736598
Saleem Khan <saleemkce@gmail.com>
2019-04-10 11:58:11 +0530

    first commit

---
+++ src/utils/validateEnv.ts
@@ -0,0 +1,16 @@
+import {
+  cleanEnv, port, str,
+} from 'envalid';
+
+function validateEnv() {
+  cleanEnv(process.env, {
+    JWT_SECRET: str(),
+    MONGO_PASSWORD: str(),
+    MONGO_PATH: str(),
+    MONGO_USER: str(),
+    PORT: port(),
+  });
+}
+
+export default validateEnv;
+// cluster0-antf0.mongodb.net/test?retryWrites=true
